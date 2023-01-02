fc92e4d7bdd8ce5d058b333c2ee9e404d6b17425
Muaaz Bdear <57080354+Mizoo1@users.noreply.github.com>
2023-01-02 14:45:19 +0100

    update

--- Galaga/Constants.cs
+++ Galaga/Constants.cs
@@ -10,8 +10,8 @@ namespace Galaga
     {
         protected Dictionary<string, string> imagePaths = new Dictionary<string, string>()
         {
-            {"background", "D:\\Muaaz\\Studim\\Semester 3\\c#\\Github\\Galaga-\\Galaga\\assets\\Background.png"},
-            {"start", "D:\\Muaaz\\Studim\\Semester 3\\c#\\Github\\Galaga-\\Galaga\\assets\\Start.png"},
+            {"background", "D:\\Muaaz\\Studim\\Semester 3\\c#\\Github\\Galaga-\\Galaga\\Assest\\Background.png"},
+            {"start", "D:\\Muaaz\\Studim\\Semester 3\\c#\\Github\\Galaga-\\Galaga\\Assest\\Start.png"},
         };
     }
 }
