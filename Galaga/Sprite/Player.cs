458ef9a52a409a35ba65d782f369af8b3b941995
Muaaz Bdear <57080354+Mizoo1@users.noreply.github.com>
2023-01-08 18:51:41 +0100

    klassen über nehmen von Nour
    ich habe deine klassen in my branch übernimmt

---
+++ Galaga/Sprite/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Galaga.Menu;
+using SDL2;
+
+namespace Galaga.Sprite
+{
+    public class Player : Sprite
+    {
+        public static bool running = true;
+        public SDL.SDL_Rect smallSprite;
+        public Input input;
+        public Player(IntPtr _texture, Input input) : base(_texture, 300, 420, 40, 40)
+        {
+            speed = 10;
+            Radius = 35;
+            this.input = input;
+        }
+        public Player(IntPtr _texture, Input input, int x, int y) : base(_texture, x, y, 40, 40)
+        {
+            speed = 10;
+            Radius = 35;
+            this.input = input;
+        }
+        public void loadContent()
+        {
+            LoadContent();
+
+            // Definieren Sie die Position und Größe der beiden Rechteckobjekte
+            smallSprite = new SDL.SDL_Rect()
+            {
+                x = 10,
+                y = 450,
+                w = 20,
+                h = 20
+            };
+        }
+        public void draw(IntPtr surface, IntPtr renderer)
+        {
+            // Erstellen Sie eine Texture aus der Surface
+            Draw(surface, renderer);
+            SDL.SDL_RenderCopy(renderer, _texture, IntPtr.Zero, ref smallSprite);
+        }
+        public bool getRunning() { return running; }
+        public SDL.SDL_Rect getSmallSprite() { return smallSprite; }
+        
+
+    }
+}
---
+++ Galaga/Sprite/Sprite.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDL2;
+
+namespace Galaga.Sprite
+{
+    public class Sprite
+    {
+        public SDL.SDL_Rect sprite;
+        public IntPtr _texture;
+        public Vector2D Velocity;
+        public int X; public int Y;
+        public int WSize; public int HSize;
+        public int Left; public int Right; public int Top; public int Bottom;
+        public float speed;
+        public float Radius;
+
+
+        public Sprite(IntPtr _texture, int x, int y, int WSize, int HSize)
+        {
+            this.WSize = WSize;
+            this.HSize = HSize;
+            this.X = x;
+            this.Y = y;
+            this._texture = _texture;
+            
+        }
+        public void LoadContent()
+        {
+            sprite = new SDL.SDL_Rect()
+            {
+                x = X,
+                y = Y,
+                w = WSize,
+                h = HSize
+            };
+            Left = X;
+            Right = X + sprite.w;
+            Top = Y;
+            Bottom = Y + sprite.h;
+        }
+        public void Draw(IntPtr surface, IntPtr renderer)
+        {
+            // Erstellen Sie eine Texture aus der Surface
+            SDL.SDL_RenderCopy(renderer, _texture, IntPtr.Zero, ref sprite); ;
+        }
+        public int getX() { return X; }
+        public int getY() { return Y; }
+        public void setX(int x) { X = x; }
+        public void setY(int y) { Y = y; }
+        public SDL.SDL_Rect getSprite() { return sprite; }
+        #region Collisoin
+        public bool IsTouchingLeft(Sprite sprite)
+        {
+            return true;
+            //TODO
+        }
+        public bool IsTouchingRight(Sprite sprite)
+        {
+            return true;
+            //TODO
+        }
+        public bool IsTouchingTop(Sprite sprite)
+        {
+            return true;
+            //TODO
+
+        }
+        public bool IsTouchingBottom(Sprite sprite)
+        {
+            return true;
+            //TODO
+        }
+
+        public float getEnemyRadius()
+        {
+            return Radius;
+        }
+        #endregion
+
+
+    }
+
+}
